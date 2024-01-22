ae5e0f708d74f77d903f0dec3927e704b85047e0
Carl Adams <carl51690@gmail.com>
2024-01-22 03:06:59 -0500

    myfile.md

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 31;
+let num2 = 2;
+const multiply = num1 * num2;
 
-// Write your code here
+const random = Math.floor(Math.random() * 100) + 1;
+
+let num3 = 4;
+let num4 = 1;
+const mod = num3 / num4;
+
+let numbers = [10, 5, 20, 15];
+let max = Math.max(...numbers);
